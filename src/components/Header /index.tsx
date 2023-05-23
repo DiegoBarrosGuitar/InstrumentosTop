f11789d0d22d52ab49f29e5ba259d820eaa8d447
Diego Barros <diegovlbarros@gmail.com>
2023-05-23 16:42:00 -0300

    Project Created

---
+++ src/components/Header /index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export default function Header() {
+    return ( 
+        <header>
+            <div className="ins-top-container">
+                <h1>Instrumentos Top</h1>
+            </div>
+        </header>
+    );
+
+}
