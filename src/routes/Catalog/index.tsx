0625f2bcfebef4e0b17314b9dc81aa9c31c2396d
Diego Barros <diegovlbarros@gmail.com>
2023-05-23 17:25:06 -0300

    SearchBar component

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,7 +1,24 @@
+import "./styles.css";
 import Header from "../../components/Header ";
+import SearchBar from "../../components/SearchBar";
 
 export default function Catalog() {
     return(
+        <>
         <Header/>
+        <main className="inst-catalog-main">
+            <section id="inst-catalog-section">
+
+                <div className="inst-search-bar-container">
+                   <SearchBar/>
+                </div>
+
+                <div className="inst-catalog-card-container">
+                </div>
+
+            </section>
+        </main>
+        </>
+        
     );
 }
